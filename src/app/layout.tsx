691f1e0b8d306f85f1459ffef2fdbe4dfef86aa6
Theo Gernke <theogernke@icloud.com>
2025-07-17 09:50:46 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next';
+import { FC, ReactNode } from 'react';
+
+import '@/styles/globals.css';
+
+const metadata: Metadata = {
+	title: 'Browser Extension Manager',
+};
+
+interface ILayoutProps {
+	children: ReactNode;
+}
+
+const Layout: FC<ILayoutProps> = ({ children }) => {
+	return (
+		<html className='overflow-hidden bg-neutral-200 dark:bg-neutral-800'>
+			<body className='font-noto-sans h-screen antialiased'>{children}</body>
+		</html>
+	);
+};
+
+export default Layout;
+export { metadata };
