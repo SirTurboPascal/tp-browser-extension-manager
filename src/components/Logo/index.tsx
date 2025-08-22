8364772cb08be417205034951f7f2ae0ddb83e54
Theo Gernke <theogernke@icloud.com>
2025-08-22 20:37:32 +0200

    Adjusts Logo props

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -7,7 +7,7 @@ export default function () {
 	return (
 		<Link className='group flex flex-row items-center gap-150' href='/'>
 			<div className='relative size-[40px] shrink-0'>
-				<Image alt='Logo' src={logoImage} fill />
+				<Image alt='Logo' src={logoImage} fill unoptimized />
 			</div>
 
 			<span className='font-geologica dark:text-neutral-0 text-[24px] font-semibold -tracking-[0.5px] whitespace-nowrap text-neutral-900 group-hover:underline'>Extensions</span>
