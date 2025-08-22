ea0b811351ee126c7557a894574359ebdbbc76df
Theo Gernke <theogernke@icloud.com>
2025-08-22 19:21:50 +0200

    Adds Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import logoImage from '@/components/Logo/logo.svg';
+
+export default function () {
+	return (
+		<Link className='group flex flex-row items-center gap-150' href='/'>
+			<div className='relative size-[40px] shrink-0'>
+				<Image alt='Logo' src={logoImage} fill />
+			</div>
+
+			<span className='font-geologica dark:text-neutral-0 text-[24px] font-semibold -tracking-[0.5px] whitespace-nowrap text-neutral-900 group-hover:underline'>Extensions</span>
+		</Link>
+	);
+}
